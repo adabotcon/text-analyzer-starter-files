a6fedb1efddf117972de2022da4b899bf79ea3fe
adabotcon <adabotcon@outlook.com>
2017-01-03 15:54:03 -0700

    modified app for text analyzer yay

--- app.js
+++ app.js
@@ -1,1 +1,72 @@
-// your code here!
+function startAnalyzeText(){
+
+	$('.js-text-form').submit(function(event){
+		event.preventDefault();
+		var textToAnalyze = $(this).find('#user-text').val();
+		analyzeText(removeReturns(textToAnalyze));
+	})
+}
+
+function analyzeText(textToAnalyze){
+	var wordArray = textToAnalyze.toLowerCase().match(/\b[^\s]+\b/g).sort();
+
+	var wordCount = wordArray.length;
+	var uniqueWordCount = calculateUniqueWords(wordArray);
+	var averageWordLength = calculateWordLength(wordArray);
+
+	var sentenceAverageLength = calculateSentenceAverageLength(textToAnalyze);
+
+	changeTextInDOM(wordCount, uniqueWordCount, averageWordLength, sentenceAverageLength);
+	
+}
+
+function tokenizeText(text) { 
+	return text.toLowerCase().match(/\b[^\s]+\b/g).sort(); 
+}
+
+function calculateUniqueWords(tokens){
+ 	var distinctWords = []; 
+ 	for (var i=0; i<tokens.length; i++) 
+ 	{
+ 		console.log(distinctWords.indexOf(tokens[i]));
+  		if (distinctWords.indexOf(tokens[i]) === -1) 
+  		{
+   			distinctWords.push(tokens[i]); 
+		} 
+	} 
+	return distinctWords.length; 
+}
+
+function calculateWordLength(wordArray){
+
+  var totalLength = wordArray.join("").length;
+  return (totalLength / wordArray.length).toFixed(2);
+
+}
+
+function calculateSentenceAverageLength(textToAnalyze){
+
+  var numSentences = textToAnalyze.match(/[.!?]+/g) ? textToAnalyze.match(/[.!?]+/g).length : 1;
+  var wordCount = tokenizeText(textToAnalyze).length;
+  return (wordCount / numSentences).toFixed(2);
+
+}
+
+function changeTextInDOM(wordCount, uniqueWordCount, averageWordLength, sentenceAverageLength){
+
+	var textAnalyzed = $('.js-text-report');
+	textAnalyzed.find('.js-word-count').text(wordCount);
+	textAnalyzed.find('.js-unique-word-count').text(uniqueWordCount);
+	textAnalyzed.find('.js-average-word-length').text(averageWordLength + " characters");
+	textAnalyzed.find('.js-average-sentence-length').text(sentenceAverageLength + " words");
+	
+	textAnalyzed.removeClass('hidden');
+}
+
+function removeReturns(text) {
+  return text.replace(/\r?\n|\r/g, "");
+}
+
+$(function(){
+	startAnalyzeText();
+});
